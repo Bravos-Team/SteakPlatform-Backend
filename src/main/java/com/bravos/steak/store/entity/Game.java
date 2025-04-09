a978d41b5f02b18532befa813a58d92759279c85
Nguyễn Quốc Bảo <nationalbao2005@gmail.com>
2025-04-09 10:53:03 +0700

    Update Game.java

--- src/main/java/com/bravos/steak/store/entity/Game.java
+++ src/main/java/com/bravos/steak/store/entity/Game.java
@@ -39,10 +39,6 @@ public class Game {
     @Enumerated(EnumType.ORDINAL)
     GameStatus status = GameStatus.OPENING;
 
-    @ManyToOne
-    @JoinColumn(name = "publisher_id")
-    Publisher publisher;
-
     @Builder.Default
     @Column(name = "created_at")
     @CreationTimestamp
